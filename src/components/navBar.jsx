b1db8ccd096f68853f75158cef4890ecbc3a264b
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-04-16 16:36:14 +0300

    new template completed

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import { getUser } from "../services/userService";
+import {getUser} from "../services/userService"
 
 class NavBar extends Component {
   state = { user: { first_name: "", last_name: "", email: "" } };
@@ -22,85 +22,32 @@ class NavBar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/">
+      <nav className="navbar navbar-expand navbar-dark bg-dark fixed-top flex-md-nowrap p-0 shadow justify-content-between">
+        <NavLink className="navbar-brand col-sm-3 col-md-2 mr-0" to="/">
           Schedu
         </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        
-            {!this.props.user_id && (
-              <div className="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav mr-auto">
+        {!this.props.user_id && (
+            <ul className="navbar-nav mr-3">
+              <li className="nav-item">
                 <NavLink className="nav-link" to="/login">
                   Login
                 </NavLink>
-                <NavLink className="nav-link" to="/register">
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-item nav-link" to="/register">
                   Register
                 </NavLink>
-              </ul>
-        </div>
-            )}
-
-        {this.props.user_id && (
-          <div
-            className="collapse navbar-collapse justify-content-between"
-            id="navbarNav"
-          >
-            <ul class="navbar-nav">
-              <NavLink
-                className="nav-link"
-                to={`/dashboard/${this.props.user_id}`}
-              >
-                Dashboard
-              </NavLink>
-              <NavLink className="nav-link" to={`/todos/${this.props.user_id}`}>
-                Objectives
-              </NavLink>
-              <NavLink
-                className="nav-link"
-                to={`/timetracker/${this.props.user_id}`}
-              >
-                TimeTracker
-              </NavLink>
-            </ul>
-            <ul className="navbar-nav">
-              <li class="nav-item dropdown dropleft">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fa fa-user-circle-o" aria-hidden="true" />
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <NavLink
-                    className="nav-link dropdown-item"
-                    to={`/profile/${this.props.user_id}`}
-                  >
-                    Profile
-                  </NavLink>
-                  <div className="dropdown-divider" />
-                  <NavLink className="nav-link dropdown-item" to="/logout">
-                    Logout
-                  </NavLink>
-                </div>
               </li>
             </ul>
-          </div>
+        )}
+        {this.props.user_id && (
+          <ul className="navbar-nav mr-3">
+            <li className="nav-item">
+              <NavLink className="nav-link text-nowrap" to="/logout">
+                Logout
+              </NavLink>
+            </li>
+          </ul>
         )}
       </nav>
     );
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -110,6 +110,9 @@ class Profile extends Form {
 
     return (
       <React.Fragment>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1>Profile</h1>
+        </div>
         <table className="table">
           <tbody>
             <tr>
