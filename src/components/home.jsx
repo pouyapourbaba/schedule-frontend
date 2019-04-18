42bfb2339309ccd976b29b7e170ca10dfeb6ce4b
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-04-18 19:31:49 +0300

    new dashboard done

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,20 +7,20 @@ class Home extends Component {
       <div>
         <h1>Welcome to Schedu</h1>
         <p>
-          Schedu is a platform where you can setup weekly goals for yourself and
-          keep the track of your tasks by storing the amount of hours that you
+          Schedu is a platform where you can set up weekly goals for yourself and
+          keep track of your tasks by storing the number of hours that you
           work on a specific task in one week.
         </p>
         <p>
           The weekly objectives can be set in the Objectives page where there
-          are optoins to edit, delete, and change the status of a goal between
+          are options to edit, delete, and change the status of a goal between
           done and not done. The TimeTracker page can be used to insert tasks
           for each week and enter the amount of time that you spent working on
           that task each day. You can see the total amount of time that you have
           worked on a specific task as well.
         </p>
         <p>
-          In the Dashboard page you can see a chart which shows the amount of
+          On the Dashboard page, you can see a chart which shows the number of
           hours that you have worked or studied during each week of the year
           2019.
         </p>
